d7dc9de9954a7e5e734462c05c1dc8c9e2e98ce3
Aleh Serhiyenia <sergienya.oleg@yandex.ru>
2019-01-07 03:18:51 +0300

    *feat and *fix add functionality, fixed bugs, add a landing page, add sounds and images

---
+++ src/screens/landing/landing.js
@@ -0,0 +1,13 @@
+import './landing.css';
+
+import './img/RSS2018q3_v3.png';
+import './img/background2.jpg';
+import './img/my-foto.jpg';
+import './img/background1.jpg';
+import './img/monsters.jpg';
+import './img/players.jpg';
+import './img/hero4.png';
+
+document.querySelector('#start').addEventListener('click', () => {
+  window.location.href = './game.html';
+});
